9b5b7f4d60bd6a8ffcab04a485b86c806a36c6dd
Unknown <fj16315@my.bristol.ac.uk>
2020-05-19 17:37:54 +0100

    Start menu behaviour added

---
+++ Assets/Scripts/ButtonCanvasControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonCanvasControl : MonoBehaviour
+{
+  public Animator startMenuStateMachine;
+
+  public void StartButton()
+  {
+    startMenuStateMachine.SetTrigger("Starting");
+  }
+
+  public void AdvanceStateMachine()
+  {
+    startMenuStateMachine.SetTrigger("Animation Complete");
+  }
+}
---
+++ Assets/Scripts/StartMenuBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuBehaviour : MonoBehaviour
+{
+  public Animator buttonCanvasAnimator;
+
+  public void ControlBehaviour(AnimatorStateInfo info)
+  {
+    if (info.IsName("Load In"))
+    {
+      buttonCanvasAnimator.SetTrigger("Fade In");
+    }
+    else if (info.IsName("Starting Game"))
+    {
+      buttonCanvasAnimator.SetTrigger("Fade Out");
+    }
+    else if (info.IsName("Load Next Scene"))
+    {
+      SceneManager.LoadScene("Main Game");
+    }
+  }
+}
